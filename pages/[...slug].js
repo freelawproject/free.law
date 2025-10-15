321b5c46d8c7f758bacd2e75dc95cdff7cde98d5
s-taube <177274951+s-taube@users.noreply.github.com>
2025-10-14 18:27:58 -0700

    Update [...slug].js

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -98,7 +98,7 @@ export default function Post({ postData, allPostsData }) {
                 financial disclosures
               </a>
               , and <a href="https://www.courtlistener.com/audio/">oral arguments</a>. We build
-              <a href="/open-source-tools/">open source tools</a> like eyecite, juriscraper, and x-ray.
+              <a href="/open-source-tools">open source tools</a> like eyecite, juriscraper, and x-ray.
             </p>
             <p>We rely on your donations for our continued success.</p>
             <p>
