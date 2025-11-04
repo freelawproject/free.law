4ece0ef0e49a5ff19832f7971f0de529b52ebffb
Elisa Anguita <elisa@free.law>
2025-11-04 20:08:57 -0300

    feat(navbar.js): clean up About popover menu

--- components/navbar.js
+++ components/navbar.js
@@ -224,37 +224,18 @@ export default function Navbar({ allPosts }) {
                       >
                         <Popover.Panel
                           static
-                          className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 left-1/2 -translate-x-1/2"
+                          className="absolute lg:left-1/2 lg:-translate-x-1/2 z-10 mt-3 transform w-screen max-w-xs rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 grid bg-white gap-2 py-5 px-4"
                         >
-                          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                              {about.map((item) => (
-                                <a
-                                  key={item.name}
-                                  href={item.href}
-                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                                >
-                                  {item.icon ? (
-                                    <item.icon
-                                      className="flex-shrink-0 h-6 w-6 text-gray-400"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <div className="h-8 w-8 relative flex-shrink-0">
-                                      <Image src={item.imgSrc} layout="fill" aria-hidden="true" />
-                                    </div>
-                                  )}
-
-                                  <div className="ml-4">
-                                    <p className="text-base font-medium text-gray-900">
-                                      {item.name}
-                                    </p>
-                                    <p className="mt-1 text-sm text-gray-500">{item.description}</p>
-                                  </div>
-                                </a>
-                              ))}
-                            </div>
-                          </div>
+                          {about.map((item) => (
+                            <Link
+                              href={item.href}
+                              key={item.name}
+                            >
+                              <a className="rounded-lg py-3 px-4 hover:bg-gray-50 text-base font-medium text-gray-900">
+                                {item.name}
+                              </a>
+                            </Link>
+                          ))}
                         </Popover.Panel>
                       </Transition>
                     </>
