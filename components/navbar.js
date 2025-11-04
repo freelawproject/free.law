9e93ff6274abfd36642d3b1b48d173904c5ca715
Elisa Anguita <elisa@free.law>
2025-11-04 20:59:09 -0300

    refactor(navbar.js): use PopoverMenu in Tools, Data and Engage menus

--- components/navbar.js
+++ components/navbar.js
@@ -267,238 +267,40 @@ export default function Navbar({ allPosts }) {
               {/* Desktop Toolbar */}
               <Popover.Group as="nav" className="hidden md:flex md:space-x-6 lg:space-x-10">
                 {/* About */}
-                <PopoverMenu items={about} title="About"></PopoverMenu>
+                <PopoverMenu items={about} title="About" width="xs"></PopoverMenu>
                 {/* End About */}
 
                 {/* Tools */}
-                <Popover className="relative">
-                  {({ open }) => (
-                    <>
-                      <Popover.Button
-                        className={classNames(
-                          open ? 'text-gray-300' : 'text-gray-100',
-                          'group rounded-md inline-flex items-center text-xs uppercase font-medium hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-offset-purple-900 focus:ring-indigo-500'
-                        )}
-                      >
-                        <span>Tools</span>
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? 'text-gray-300' : 'text-gray-100',
-                            'ml-1 h-4 w-4 group-hover:text-gray-200'
-                          )}
-                          aria-hidden="true"
-                        />
-                      </Popover.Button>
-
-                      <Transition
-                        show={open}
-                        as={Fragment}
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 translate-y-1"
-                        enterTo="opacity-100 translate-y-0"
-                        leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100 translate-y-0"
-                        leaveTo="opacity-0 translate-y-1"
-                      >
-                        <Popover.Panel
-                          static
-                          className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 left-1/2 -translate-x-1/2"
+                <PopoverMenu items={tools} title="Tools">
+                  <div className="rounded-b-lg px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+                    {callsToAction.map((item) => (
+                      <div key={item.name} className="flow-root">
+                        <a
+                          href={item.href}
+                          className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
                         >
-                          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                              {tools.map((item) => (
-                                <a
-                                  key={item.name}
-                                  href={item.href}
-                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                                >
-                                  {item.icon ? (
-                                    <item.icon
-                                      className="flex-shrink-0 h-6 w-6 text-gray-400"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <div className="h-8 w-8 relative flex-shrink-0">
-                                      <Image src={item.imgSrc} layout="fill" aria-hidden="true" />
-                                    </div>
-                                  )}
-
-                                  <div className="ml-4">
-                                    <p className="text-base font-medium text-gray-900">
-                                      {item.name}
-                                    </p>
-                                    <p className="mt-1 text-sm text-gray-500">{item.description}</p>
-                                  </div>
-                                </a>
-                              ))}
-                            </div>
-                            <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                              {callsToAction.map((item) => (
-                                <div key={item.name} className="flow-root">
-                                  <a
-                                    href={item.href}
-                                    className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
-                                  >
-                                    <item.icon
-                                      className="flex-shrink-0 h-6 w-6 text-gray-400"
-                                      aria-hidden="true"
-                                    />
-                                    <span className="ml-3">{item.name}</span>
-                                  </a>
-                                </div>
-                              ))}
-                            </div>
-                          </div>
-                        </Popover.Panel>
-                      </Transition>
-                    </>
-                  )}
-                </Popover>
+                          <item.icon
+                            className="flex-shrink-0 h-6 w-6 text-gray-400"
+                            aria-hidden="true"
+                          />
+                          <span className="ml-3">{item.name}</span>
+                        </a>
+                      </div>
+                    ))}
+                  </div>
+                </PopoverMenu>
                 {/* End Tools */}
 
                 {/* Data */}
-                <Popover className="relative">
-                  {({ open }) => (
-                    <>
-                      <Popover.Button
-                        className={classNames(
-                          open ? 'text-gray-300' : 'text-gray-100',
-                          'group rounded-md inline-flex items-center text-xs uppercase font-medium hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-offset-purple-900 focus:ring-indigo-500'
-                        )}
-                      >
-                        <span>Data</span>
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? 'text-gray-300' : 'text-gray-100',
-                            'ml-1 h-4 w-4 group-hover:text-gray-200'
-                          )}
-                          aria-hidden="true"
-                        />
-                      </Popover.Button>
-
-                      <Transition
-                        show={open}
-                        as={Fragment}
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 translate-y-1"
-                        enterTo="opacity-100 translate-y-0"
-                        leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100 translate-y-0"
-                        leaveTo="opacity-0 translate-y-1"
-                      >
-                        <Popover.Panel
-                          static
-                          className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 left-1/2 -translate-x-1/2"
-                        >
-                          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                              {data.map((item) => (
-                                <a
-                                  key={item.name}
-                                  href={item.href}
-                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                                >
-                                  {item.icon ? (
-                                    <item.icon
-                                      className="flex-shrink-0 h-6 w-6 text-gray-400"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <div
-                                      className={`${
-                                        item.icon_height ? item.icon_height : 'h-6'
-                                      } w-6 relative flex-shrink-0`}
-                                    >
-                                      <img src={item.imgSrc} aria-hidden="true" />
-                                    </div>
-                                  )}
-                                  <div className="ml-4">
-                                    <p className="text-base font-medium text-gray-900">
-                                      {item.name}
-                                    </p>
-                                    <p className="mt-1 text-sm text-gray-500">{item.description}</p>
-                                  </div>
-                                </a>
-                              ))}
-                            </div>
-                            <DropDownCallToAction href="https://github.com/freelawproject/">
-                              View More Projects on Github
-                            </DropDownCallToAction>
-                          </div>
-                        </Popover.Panel>
-                      </Transition>
-                    </>
-                  )}
-                </Popover>
+                <PopoverMenu items={data} title="Data">
+                  <DropDownCallToAction href="https://github.com/freelawproject/">
+                    View More Projects on Github
+                  </DropDownCallToAction>
+                </PopoverMenu>
                 {/* End Data */}
 
                 {/* Engage */}
-                <Popover className="relative">
-                  {({ open }) => (
-                    <>
-                      <Popover.Button
-                        className={classNames(
-                          open ? 'text-gray-300' : 'text-gray-100',
-                          'group rounded-md inline-flex items-center text-xs uppercase font-medium hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-offset-purple-900 focus:ring-indigo-500'
-                        )}
-                      >
-                        <span>Data</span>
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? 'text-gray-300' : 'text-gray-100',
-                            'ml-1 h-4 w-4 group-hover:text-gray-200'
-                          )}
-                          aria-hidden="true"
-                        />
-                      </Popover.Button>
-
-                      <Transition
-                        show={open}
-                        as={Fragment}
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 translate-y-1"
-                        enterTo="opacity-100 translate-y-0"
-                        leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100 translate-y-0"
-                        leaveTo="opacity-0 translate-y-1"
-                      >
-                        <Popover.Panel
-                          static
-                          className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 left-1/2 -translate-x-1/2"
-                        >
-                          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                              {engage.map((item) => (
-                                <a
-                                  key={item.name}
-                                  href={item.href}
-                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                                >
-                                  {item.icon ? (
-                                    <item.icon
-                                      className="flex-shrink-0 h-6 w-6 text-purple-800"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <div className="h-6 w-6 relative flex-shrink-0">
-                                      <Image src={item.imgSrc} layout="fill" aria-hidden="true" />
-                                    </div>
-                                  )}
-                                  <div className="ml-4">
-                                    <p className="text-base font-medium text-gray-900">
-                                      {item.name}
-                                    </p>
-                                    <p className="mt-1 text-sm text-gray-500">{item.description}</p>
-                                  </div>
-                                </a>
-                              ))}
-                            </div>
-                          </div>
-                        </Popover.Panel>
-                      </Transition>
-                    </>
-                  )}
-                </Popover>
+                <PopoverMenu items={engage} title="Engage" width="xs"></PopoverMenu>
                 {/* End Engage */}
 
                 {/* Blog */}
